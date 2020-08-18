3afa58d14530a925cb959bc7f6c83d96c368f922
saqo <sevad999@mail.ru>
2020-08-18 18:35:56 +0400

    Create wwwroot folder with css js folders and files

--- Shop/Data/Mocks/MockCars.cs
+++ Shop/Data/Mocks/MockCars.cs
@@ -18,35 +18,35 @@ namespace Shop.Data.Mocks
                 {
                     name="Tesla Model S",shortDesc="Fast car",longtDesc="Beutiful ,fast and very qouet car company of Tesla",
                     price=45000,isFavorite=true,available=true,
-                    img="",
+                    img="/img/tesla.webp",
                     Category=_categor.AllCategories.FirstOrDefault(x=>x.categoryName=="electric")
                 },
                 new Car
                 {
                     name="Ford Fiesta",shortDesc="Quiet and ...",longtDesc="comfotable car for daily live",
                     price=11000,isFavorite=false,available=true,
-                    img="",
+                    img="/img/ford.jpg",
                     Category=_categor.AllCategories.Last()
                 },
                  new Car
                 {
                     name="BMW M3",shortDesc="strang ",longtDesc="comforatable and large car for daily live",
                     price=65000,isFavorite=true,available=true,
-                    img="",
+                    img="/img/m3.jpg",
                     Category=_categor.AllCategories.Last()
                 },
                  new Car
                 {
                     name="Mercedes S",shortDesc="comforatable and large",longtDesc="comforatable and large car for daily live",
                     price=40000,isFavorite=false,available=false,
-                    img="",
+                    img="/img/mercedes.jpeg",
                     Category=_categor.AllCategories.Last()
                 },
                  new Car
                 {
                     name="Nissan Leaf",shortDesc="quiet and economic ",longtDesc="comforatable and large car for daily live",
                     price=14000,isFavorite=true,available=true,
-                    img="",
+                    img="/img/nissan.jpg",
                     Category=_categor.AllCategories.First()
                 }
             };
