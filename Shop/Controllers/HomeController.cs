68e22315b35288806292d6d70c4861b05d8c3686
saqo <sevad999@mail.ru>
2020-08-24 00:04:23 +0400

    Add HomeController to show fav items

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+        public HomeController(IAllCars carRepository)
+        {
+            _carRep = carRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeCars = new HomeViewModel()
+            {
+                favCars = _carRep.GetFavCars
+            };
+
+            return View(homeCars);
+        }
+    }
+}
--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -66,7 +66,7 @@ namespace Shop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Cars}/{action=List}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
             using (var scoope = app.ApplicationServices.CreateScope())
             {
